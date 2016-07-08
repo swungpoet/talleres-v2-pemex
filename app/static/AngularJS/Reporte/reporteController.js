7b52a6dcc82e2ecdde24e0551d35b893738b21de
urielgangster <porkpie_mod@hotmail.com>
2016-07-08 18:51:47 -0500

    Reporte previo
    Reporte previo

--- app/static/AngularJS/Reporte/reporteController.js
+++ app/static/AngularJS/Reporte/reporteController.js
@@ -1,15 +1,49 @@
 registrationModule.controller('reporteController', function ($scope, alertFactory, $rootScope, localStorageService, reporteRepository) {
+    $scope.message = "Buscando...";
     
     $scope.init = function () {
-        getReporte();
+        $scope.getReporte();
     }
 
     $scope.getReporte = function () {
+        $scope.promise =
         reporteRepository.reporteGral()
             .then(function (result) {
-                if (result.data.length > 0) {
-                    alertFactory.success("Reposte general cargado");
+                if (result.data.length > 0) {                    
                     $scope.datos = result.data;
+                    
+                    setTimeout(function () {
+                            $('.dataTableReporte').DataTable({
+                                buttons: [
+                                    {
+                                        extend: 'copy'
+                                    },
+                                    {
+                                        extend: 'csv'
+                                    },
+                                    {
+                                        extend: 'excel',
+                                        title: 'ExampleFile'
+                                    },
+                                    {
+                                        extend: 'pdf',
+                                        title: 'ExampleFile'
+                                    },
+
+                                    {
+                                        extend: 'print',
+                                        customize: function (win) {
+                                            $(win.document.body).addClass('white-bg');
+                                            $(win.document.body).css('font-size', '10px');
+
+                                            $(win.document.body).find('table')
+                                                .addClass('compact')
+                                                .css('font-size', 'inherit');
+                                        }
+                            }
+                        ]
+                            });
+                        }, 1000);
                 } 
             }, function (error) {
                 alertFactory.error('Error');
--- app/static/AngularJS/Reporte/reporteRepository.js
+++ app/static/AngularJS/Reporte/reporteRepository.js
@@ -1,4 +1,4 @@
-var loginUrl = global_settings.urlCORS + '/api/reporte/';
+var searchUrl = global_settings.urlCORS + '/api/reporte/';
 
 registrationModule.factory('reporteRepository', function ($http) {
     return {        
