52332b7d3e37205a3fffad0bdf47407beb05e32b
Adolfo Martinez <swungpoet@hotmail.com>
2016-07-13 17:57:17 -0500

    ordenservicioevidencias por cotizacion
    se agrega tipo usuario para filtrar los documentos que ve el usuario
    firmado

--- app/static/AngularJS/Orden/ordenServicioEvidenciaRepository.js
+++ app/static/AngularJS/Orden/ordenServicioEvidenciaRepository.js
@@ -2,12 +2,12 @@ var searchUrl = global_settings.urlCORS + '/api/cotizacion/';
 
 registrationModule.factory('ordenServicioEvidenciaRepository', function ($http) {
     return {
-        getEvidenciasByOrden: function (idTrabajo) {
+        getEvidenciasByOrden: function (idTrabajo, idTipoUsuario) {
             return $http({
                 url: searchUrl + 'evidenciasByOrden',
                 method: "GET",
                 params: {
-                    idTrabajo: idTrabajo
+                    idTrabajo: idTrabajo, idTipoUsuario:idTipoUsuario
                 },
                 headers: {
                     'Content-Type': 'application/json'
