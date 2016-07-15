a9fdeb988aef7ed45400997182a0f1f248039f55
Vladimir Juarez <vladimouse@gmail.com>
2016-07-15 12:45:04 -0500

    cambio en cotizacion
    :+1:

--- app/website/controllers/cotizacion.js
+++ app/website/controllers/cotizacion.js
@@ -27,7 +27,6 @@ var Cotizacion = function (conf) {
 
 var storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        var dirname = 'C:/Desarrollo/Talleres/talleres-v2-pemex/app/static/uploads/files/';
         var idTrabajo = req.body.idTrabajo;
         var idCotizacion = req.body.idCotizacion;
 
@@ -486,7 +485,6 @@ Cotizacion.prototype.post_uploadfiles = function (req, res, next) {
     res.end("File is uploaded");
     
     //directorio de los archivos
-    var dirname = 'C:/Desarrollo/Talleres/talleres-v2-pemex/app/static/uploads/files/';
     //Objeto que almacena la respuesta
     var object = {};
     //Referencia a la clase para callback
@@ -638,7 +636,6 @@ Cotizacion.prototype.get_namefileserver = function (req, res, next) {
     var object = {};
     //Referencia a la clase para callback
     var self = this;
-    var dirname = 'C:/Desarrollo/Talleres/talleres-v2-pemex/app/static/uploads/files/'; 
         //Callback
         object.error = null;
         object.result = getNameFile(dirname + req.query.idTrabajo + '/certificadoConformidad/');
