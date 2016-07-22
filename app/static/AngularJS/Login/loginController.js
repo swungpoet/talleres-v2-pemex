b94372bcf88204e212cfe100535941bf3e329197
Vladimir Juarez <vladimouse@gmail.com>
2016-07-22 13:39:16 -0500

    cambios en login controller
    :+1:

--- app/static/AngularJS/Login/loginController.js
+++ app/static/AngularJS/Login/loginController.js
@@ -1,10 +1,8 @@
-registrationModule.controller('loginController', function ($scope, alertFactory, $rootScope, localStorageService, loginRepository) {
+registrationModule.controller('loginController', function ($scope, alertFactory, $rootScope, localStorageService, loginRepository, $route, citaRepository) {
     $rootScope.sesion = 0;
     $rootScope.showChat = 0;
-    
-    $scope.init = function () {
 
-    }
+    $scope.init = function () {}
 
     $scope.login = function (username, password) {
         loginRepository.login(username, password)
@@ -13,7 +11,18 @@ registrationModule.controller('loginController', function ($scope, alertFactory,
                     alertFactory.success('Bienvenido a Talleres: ' + result.data[0].nombreCompleto);
                     $scope.login = result.data[0];
                     localStorageService.set('userData', $scope.login);
-
+                    if ($route.current.params.confCita != undefined) {
+                        var idConfCita = Number($route.current.params.confCita);
+                        if (idConfCita != 0) {
+                            citaRepository.validaConfirmacionCita(idConfCita).then(function (exists) {
+                                if (exists.data[0].existe == 1) {
+                                    alertFactory.info("La cita ya ha sido confirmada");
+                                } else {
+                                    alertFactory.success("Cita confirmada");
+                                }
+                            });
+                        }
+                    }
                     if ($scope.login.idTipoUsuario == 3) {
                         location.href = '/tallercita';
                     } else {
