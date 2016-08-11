35c5cf3df6ee4d122fc7a0dbd0aa857de156d4d0
Vladimir Juarez <vladimouse@gmail.com>
2016-08-11 11:08:31 -0500

    rename de evidencias de cotización
    :+1:

--- app/website/controllers/cotizacion.js
+++ app/website/controllers/cotizacion.js
@@ -11,6 +11,7 @@ var dirCopades = 'C:/Produccion/Talleres/talleres-v2-pemex/app/static/uploads/co
 var nameFile = '';
 var idTrabajo = 0;
 var idNombreEspecial = 0;
+var consecutivoArchivo = 0;
 
 var Cotizacion = function (conf) {
     this.conf = conf || {};
@@ -526,8 +527,10 @@ Cotizacion.prototype.post_uploadfiles = function (req, res, next) {
                 }
 
                 if (file.mimetype == 'image/jpeg' || file.mimetype == 'image/png' || file.mimetype == 'image/gif' || file.mimetype == 'image/jpg' || file.mimetype == 'image/bmp' || file.mimetype == 'video/mp4') {
+                    consecutivoArchivo = obtieneConsecutivo(dirname + idTrabajo + '/' + idCotizacion+'/multimedia');
                     cb(null, dirname + idTrabajo + '/' + idCotizacion + '/multimedia')
                 } else {
+                    consecutivoArchivo = obtieneConsecutivo(dirname + idTrabajo + '/' + idCotizacion+'/documentos');
                     cb(null, dirname + idTrabajo + '/' + idCotizacion + '/documentos')
                 }
             }else{
@@ -544,9 +547,12 @@ Cotizacion.prototype.post_uploadfiles = function (req, res, next) {
                 cb(null, nameFile + obtenerExtArchivo(file.originalname));
             }
             else{
-                cb(null, file.originalname);
+                if(consecutivoArchivo > 0){
+                    cb(null, 'Evidencia'+consecutivoArchivo+obtenerExtArchivo(file.originalname));   
+                }
             }
             nameFile = '';
+            consecutivoArchivo = 0;
         }
     });
     var upload = multer({
