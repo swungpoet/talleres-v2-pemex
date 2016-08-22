d7ec7be2a6621327a6ed795f2b7d5ad3a856e224
Vladimir Juarez <vladimouse@gmail.com>
2016-08-22 12:21:15 -0500

    nuevos métodos para taller
    :+1:

--- app/website/controllers/taller.js
+++ app/website/controllers/taller.js
@@ -60,6 +60,48 @@ Taller.prototype.get_obtienetalleres = function (req, res, next) {
     });
 }
 
+//obtiene el tipo de persona
+Taller.prototype.get_obtienetipopersona = function (req, res, next) {
+    //Con req.query se obtienen los parametros de la url
+    //Ejemplo: ?p1=a&p2=b
+    //Retorna {p1:'a',p2:'b'}
+    //Objeto que envía los parámetros
+    //var params = [];
+    //Referencia a la clase para callback
+    var self = this;
+
+    //asignación de valores mediante parámetros del request
+    var params = [];
+
+    this.model.query('SEL_TIPO_PERSONA_SP', params, function (error, result) {
+        self.view.expositor(res, {
+            error: error,
+            result: result
+        });
+    });
+}
+
+//obtiene el estatus del taller
+Taller.prototype.get_obtieneestatustaller = function (req, res, next) {
+    //Con req.query se obtienen los parametros de la url
+    //Ejemplo: ?p1=a&p2=b
+    //Retorna {p1:'a',p2:'b'}
+    //Objeto que envía los parámetros
+    //var params = [];
+    //Referencia a la clase para callback
+    var self = this;
+
+    //asignación de valores mediante parámetros del request
+    var params = [];
+
+    this.model.query('SEL_ESATUS_TALLER_SP', params, function (error, result) {
+        self.view.expositor(res, {
+            error: error,
+            result: result
+        });
+    });
+}
+
 //inserta nuevo taller
 Taller.prototype.post_insertataller = function (req, res, next) {
     //Referencia a la clase para callback
@@ -79,7 +121,6 @@ Taller.prototype.post_insertataller = function (req, res, next) {
         });
     });
 }
-
 
 //actualiza taller existente
 Taller.prototype.post_actualizataller = function (req, res, next) {
