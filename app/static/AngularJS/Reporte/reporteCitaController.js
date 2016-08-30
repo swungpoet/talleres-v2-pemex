01a7f23b13021ccbfff479ded7b577c53a39263c
Adolfo Martinez <swungpoet@hotmail.com>
2016-08-30 13:02:29 -0500

    Devuelve el valor de cita segun el estatus
    citaAgendada(0)
    citaConfirmada(1)
    citaCancelada(2)

--- app/static/AngularJS/Reporte/reporteCitaController.js
+++ app/static/AngularJS/Reporte/reporteCitaController.js
@@ -6,25 +6,52 @@
 
 registrationModule.controller('reporteCitaController', function ($scope, alertFactory, $rootScope, localStorageService, reporteCitaRepository) {
 
+    //Inicializa la pagina
     $scope.init = function () {
-     $scope.getNumeroCitas();
+        $scope.obtieneDatoUrl();
+        $scope.getNumeroCitas();
     }
 
-    //OBTIENE EL TOTAL DE CITAS
+    //obtiene el total de las citas
     $scope.getNumeroCitas = function () {
-      reporteCitaRepository.getNumCita().then(function (citas) {
-            $scope.registroCitas = citas.data;
-            $scope.citasagendadas = $scope.registroCitas[0].total;
-            $scope.citasconfirmadas = $scope.registroCitas[1].total;
-            $scope.citascanceladas = $scope.registroCitas[2].total;
-            if (citas.data.length > 0) {
-                alertFactory.success('Datos encontrados');
-            } else {
-                alertFactory.info('No se encontraron datos');
-            }
-        }, function (error) {
-            alertFactory.error('Error al obtener los datos');
-        });
+            reporteCitaRepository.getNumCita().then(function (citas) {
+                $scope.registroCitas = citas.data;
+                $scope.citasagendadas = $scope.registroCitas[0].total;
+                $scope.citasconfirmadas = $scope.registroCitas[1].total;
+                $scope.citascanceladas = $scope.registroCitas[2].total;
+                if (citas.data.length > 0) {
+                    alertFactory.success('Datos encontrados');
+                } else {
+                    alertFactory.info('No se encontraron datos');
+                }
+            }, function (error) {
+                alertFactory.error('Error al obtener los datos');
+            });
+        }
+        //Muestra el historico de citas canceldas
+    $scope.citaCancelada = function (status) {
+            $scope.tipoCita = status;
+        }
+        //Muestra el historico de citas confirmadas
+    $scope.citaConfirmada = function (status) {
+            $scope.tipoCita = status;
+        }
+        //Muestra el historico de citas agendadas
+    $scope.citaAgendada = function (status) {
+        $scope.tipoCita = status;
     }
+
+    //Regresa la variable de la url
+    $scope.obtieneDatoUrl = function () {
+        var url = location.search.replace("?", "");
+        var arrUrl = url.split("&");
+        var urlObj = {};
+        for (var i = 0; i < arrUrl.length; i++) {
+            var x = arrUrl[i].split("=");
+            urlObj[x[0]] = x[1]
+        }
+        $scope.tipoCita = urlObj.tipoCita;
+    }
+
 
 });
