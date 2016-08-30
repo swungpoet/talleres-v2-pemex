1e651b58afeb94acfe28c93ac75851ad009c225a
Adolfo Martinez <swungpoet@hotmail.com>
2016-08-30 11:59:33 -0500

    metodo get sumatoriaCitas
    metodo get sumatoriaCitas

--- app/static/AngularJS/Reporte/reporteCitaRepository.js
+++ app/static/AngularJS/Reporte/reporteCitaRepository.js
@@ -2,7 +2,9 @@ var reporteCitaUrl = global_settings.urlCORS + '/api/dashboard/';
 
 registrationModule.factory('reporteCitaRepository', function ($http) {
     return {        
+        getNumCita: function () {
             return $http({
+                url: reporteCitaUrl + 'sumatoriaCitas/',
                 method: "GET",
                 headers: {
                     'Content-Type': 'application/json'
