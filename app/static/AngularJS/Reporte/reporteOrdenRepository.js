3163c0f7290c85ab024a1d06d106a86179291fb2
Adolfo Martinez <swungpoet@hotmail.com>
2016-08-30 19:10:19 -0500

    Vista para reporte de citas por dashboard
    Vista para reporte de citas por dashboard

--- app/static/AngularJS/Reporte/reporteOrdenRepository.js
+++ app/static/AngularJS/Reporte/reporteOrdenRepository.js
@@ -2,7 +2,15 @@ var reporteOrdenUrl = global_settings.urlCORS + '/api/dashboard/';
 
 registrationModule.factory('reporteOrdenRepository', function ($http) {
     return {        
-
+        getNumOrdenes: function () {
+            return $http({
+                url: reporteOrdenUrl + 'sumatoriaOrdenes/',
+                method: "POST",
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            });
+        }
 
     };
 });
