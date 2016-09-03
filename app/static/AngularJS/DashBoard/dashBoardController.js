f26991719fe2c02b7525f4a5d8a2983fbb62b273
bigpada <anta0101@hotmail.com>
2016-09-02 19:03:13 -0500

    citas dashboard

--- app/static/AngularJS/DashBoard/dashBoardController.js
+++ app/static/AngularJS/DashBoard/dashBoardController.js
@@ -28,10 +28,10 @@ registrationModule.controller('dashBoardController', function ($scope, alertFact
                 $scope.totalHoras = 0;
 
                 datos.data.forEach(function (sumatoria) {
-                        if (sumatoria.estatus == 'SOLICITADAS') solicitadas = sumatoria.total;
-                        if (sumatoria.estatus == 'AGENDADA') agendadas = sumatoria.total;
-                        if (sumatoria.estatus == 'CONFIRMADA') confirmadas = sumatoria.total;
-                        if (sumatoria.estatus == 'CANCELADA') canceladas = sumatoria.total;
+                        if (sumatoria.estatus == 'Solicitadas') solicitadas = sumatoria.total;
+                        if (sumatoria.estatus == 'Agendadas') agendadas = sumatoria.total;
+                        if (sumatoria.estatus == 'Confirmadas') confirmadas = sumatoria.total;
+                        if (sumatoria.estatus == 'Canceladas') canceladas = sumatoria.total;
 
                         $scope.totalHoras = $scope.totalHoras + sumatoria.promedio;
 
