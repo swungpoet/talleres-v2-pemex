fd38cea5d5b52e7055befbb02428c04bde075a73
Candi1 <aperez@bism.com.mx>
2016-11-10 13:53:49 -0600

    Ajuste para aprobacion

--- app/static/AngularJS/modals/Controller/token_controller.js
+++ app/static/AngularJS/modals/Controller/token_controller.js
@@ -11,10 +11,13 @@ registrationModule.controller('token_controller', function ($scope, $modalInstan
 
         if (origen == 'Aprobacion') {
             $scope.show_Aprobacion= true;
+            $scope.idUsuario= 190;
         }else if (origen == 'Cita'){
             $scope.show_cita= true; 
+            $scope.idUsuario= 190;
         }else if (origen == 'Trabajo'){
             $scope.show_trabajo= true; 
+            $scope.idUsuario= 47;
         }
 	}
 	 $scope.close = function () {
@@ -23,21 +26,27 @@ registrationModule.controller('token_controller', function ($scope, $modalInstan
 
     $scope.saveToken = function () {
         ordenServicioRepository.estatusToken($scope.token).then(function (estatus) {
-      
             if (estatus.data.length > 0) {
                 if (estatus.data[0].estatus == 1) {
-                  
-                    ordenServicioRepository.putAprobacionUtilidadRespuesta(idAprobacionUtilidad,$scope.userData.idUsuario, $scope.token).then(function (aprobacionUtilidad) {
+
+                    if (estatus.data[0].idUsuario == $scope.idUsuario ) {
+
+                        ordenServicioRepository.putAprobacionUtilidadRespuesta(idAprobacionUtilidad,$scope.userData.idUsuario, $scope.token).then(function (aprobacionUtilidad) {
                      
-                        if (aprobacionUtilidad.data[0].id > 0) {
-                              
-                            alertFactory.success("Proceso Realizado!");   
-                            callback();   
-                            $scope.close();                         
-                        }
-                    }, function (error) {
-                        alertFactory.error("Error al cargar la orden");
-                    });
+                            if (aprobacionUtilidad.data[0].id > 0) {
+                                  
+                                alertFactory.success("Proceso Realizado!");   
+                                callback();   
+                                $scope.close();                         
+                            }
+                        }, function (error) {
+                            alertFactory.error("Error al aprobar la orden");
+                        });
+
+                    }else{
+                        alertFactory.error("El token no corresponde al usuario."); 
+                    }
+        
                 }else{
                     alertFactory.error("El token se ha utilizado previamente."); 
                 }
