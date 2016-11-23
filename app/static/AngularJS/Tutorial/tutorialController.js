fc20fe1db25c88776f88a93c20c661cfac199650
Candi1 <aperez@bism.com.mx>
2016-11-23 13:18:19 -0600

    tutorial

--- app/static/AngularJS/Tutorial/tutorialController.js
+++ app/static/AngularJS/Tutorial/tutorialController.js
@@ -1,16 +1,31 @@
 // -- =============================================
-// -- Author:      Uriel Godínez Martínez
-// -- Create date: 23/03/2016
-// -- Description: Citas controller
-// -- Modificó: V. Vladimir Juárez Juárez
-// -- Fecha: 30/03/2016
-// -- Modificó: Carlos Adolfo Martinez Diosdado
-// -- Fecha: 08/07/2016
+// -- Author:      Anel Candi Pérez Pérez
+// -- Create date: 23/11/2016
+// -- Description: tutorial controller
 // -- =============================================
 
 registrationModule.controller('tutorialController', function ($scope, $route, $modal, $rootScope, localStorageService, alertFactory, globalFactory, citaRepository, ordenServicioRepository, cotizacionRepository, trabajoRepository, uploadRepository) {
- 
-	$scope.videoTutorial = function () {
-        window.open($rootScope.vIpServer + '/uploads/tutorial/consulta_de_citas.mp4', '_blank', 'Cita');
+ 	
+ 	$scope.show_slide=false;
+ 	$scope.class_info="animate_off";
+ 	$scope.class_width="content_info";
+ 	$scope.class_info_width="";
+
+	$scope.info = function () {
+		if ($scope.show_slide==false) {
+			$scope.show_slide=true;
+	        $scope.class_info="animate_on";
+	        $scope.class_width="content_width";
+	        $scope.class_info_width="info_width";
+		}else{
+			$scope.show_slide=false;
+	        $scope.class_info="animate_off";
+		 	$scope.class_width="content_info";
+		 	$scope.class_info_width="";
+		}
      }
+
+    $scope.descargaTeam = function () {
+    	window.open($rootScope.vIpServer + '/uploads/tutorial/TeamViewer.exe');
+    }
 });
