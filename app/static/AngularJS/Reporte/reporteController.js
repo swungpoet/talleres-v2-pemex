867c1bb5738ed84e04797fbcc33a3d5f6c41b6d9
RaulArce <rarce@bism.com.mx>
2017-03-07 10:26:02 -0600

    Reporte Filtros Antiguedad Saldos

--- app/static/AngularJS/Reporte/reporteController.js
+++ app/static/AngularJS/Reporte/reporteController.js
@@ -1,9 +1,21 @@
-registrationModule.controller('reporteController', function ($scope, alertFactory, $rootScope, localStorageService, reporteRepository) {
+registrationModule.controller('reporteController', function ($scope, alertFactory, $rootScope, localStorageService, ordenPorCobrarRepository, reporteRepository,dashBoardRepository) {
     $scope.message = "Buscando...";
     
     $scope.init = function () {
         $scope.getReporte();
         $rootScope.userData = localStorageService.get('userData');
+
+        $scope.devuelveZonas();
+        $scope.devuelveTars();
+    }
+
+    $scope.fechaRango = function () {
+        $scope.fechaMes = null;
+    }
+
+    $scope.fechaMess = function () {
+        $scope.fechaInicio = null;
+        $scope.fechaFin = null;
     }
 
     $scope.getReporte = function () {
@@ -41,5 +53,49 @@ registrationModule.controller('reporteController', function ($scope, alertFactor
                 alertFactory.error('Error');
             });
     }
+
+    
+$scope.devuelveTars = function (zona) {
+        if (zona != null) {
+            dashBoardRepository.getTars(zona).then(function (tars) {
+                if (tars.data.length > 0) {
+                    $scope.tars = tars.data;
+
+                }
+            }, function (error) {
+                alertFactory.error('No se pudo recuperar información de las TARs');
+            });
+        } else {
+            $scope.tar = null;
+        }
+    }
+
+$scope.devuelveZonas = function () {
+        dashBoardRepository.getZonas($scope.userData.idUsuario).then(function (zonas) {
+            if (zonas.data.length > 0) {
+                $scope.zonas = zonas.data;
+
+            }
+        }, function (error) {
+            alertFactory.error('No se pudo recuperar información de las zonas');
+        });
+    }
+
+$scope.buscaCallCenter = function () {
+        reporteRepository.getCallcenter().then(function () {
+            if (callCenter.data.length > 0) {
+                $scope.callCenter = callCenter.data;
+
+            }
+        }, function (error) {
+            alertFactory.error('No se pudo recuperar información de las zonas');
+        });
+    }
+
+
+  
+
+       
+    
 });
 
