2cecb19b5b2e775da8084b3659803a2dbedfd46e
swungpoet <swungpoet@hotmail.com>
2017-07-27 12:31:00 -0500

    modal Presupuesto

---
+++ app/static/AngularJS/modals/Controller/presupuestoController.js
@@ -0,0 +1,8 @@
+
+registrationModule.controller('presupuestoController', function ($scope, $modalInstance, $modal, idAprobacionUtilidad, origen, callback, error, $http, $sce, $window, ordenServicioRepository, alertFactory) {
+	
+	$scope.init= function (){
+
+	}
+
+});	
